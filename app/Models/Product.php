9942143022dd54e5466b993d7deadc95270c3609
Arsya-Fisa <arsyafikrisabilillah10@gmail.com>
2026-09-24 21:36:55 +0700

    tambah relasi belongsToMany product ke transactions

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,11 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany; // 1. Tambahkan use ini di atas
 
 class Product extends Model
 {
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    // 2. Tambahkan method transactions dengan relasi belongsToMany
+    public function transactions(): BelongsToMany
+    {
+        return $this->belongsToMany(Transaction::class, 'transaction_details');
     }
 }
